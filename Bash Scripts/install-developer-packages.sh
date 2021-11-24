ea7f1bb302fed54afa0580b54fa99d5c5cacf517
TheRambotnic <lucasrafaellonguini09@gmail.com>
2021-11-23 21:59:03 -0300

    added conditional to check if Snap Store is installed and command to clean APT cache

--- Bash Scripts/install-developer-packages.sh
+++ Bash Scripts/install-developer-packages.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-# before using this script, run this command in the terminal: chmod a+x ./install-developer-packages.sh
 
 #
 # @author 	Lucas "Rambotnic" Rafael
-# @updated	November 20, 2021
+# @updated	November 23, 2021
 #
 
 # text colors
-declare normal="\033[1;37m" # white
+declare default="\033[1;0m" # default color
 declare install="\033[1;33m" # yellow
 declare finished="\033[1;32m" # green
 
 # Visual Studio Code
-echo -e "${install}==============================="
-echo -e " Installing Visual Studio Code..."
-echo -e "===============================${normal}"
+echo -e "${install}==================================\n\n Installing Visual Studio Code... \n\n==================================${default}"
 sleep 2
+# check to see if Snap Store is installed
+declare isSnapInstalled=$(dpkg-query -W -f='${Status}' snapd 2>/dev/null | grep -c "ok installed")
+if [ $isSnapInstalled = 0 ]; then
+	echo "Snap Store was not found. Installing Snap..."
+	sudo apt-get update
+	sudo apt-get install snapd
+	sleep 2
+fi
 sudo snap install --classic code
 
 # Developer packages
@@ -25,9 +30,7 @@ declare -a devPkgs=(
 )
 
 for pkg in "${devPkgs[@]}"; do
-	echo -e "${install}\n==============================="
-	echo -e " Installing ${pkg}"
-	echo -e "===============================\n${normal}"
+	echo -e "${install}\n==================================\n\n Installing ${pkg} \n\n==================================${default}"
 	sleep 2
 	sudo apt-get install $pkg
 done
@@ -37,9 +40,10 @@ sudo npm cache clean -f
 sudo npm install -g n
 sudo n latest
 
-echo -e "${install}\n*** Removing unused dependencies... ***\n${normal}"
+echo -e "${install}\n*** Removing unused dependencies... ***\n${default}"
 sleep 2
 sudo apt autoremove
+sudo apt-get clean
 
-echo -e "\n${finished}ALL DONE! :)${normal}\n"
+echo -e "\n${finished}ALL DONE! :)${default}\n"
 read # pause execution
