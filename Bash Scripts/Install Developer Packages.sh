f399c9218da379f3b00162442fc0b2ac1dc62b3f
TheRambotnic <lucasrafaellonguini09@gmail.com>
2022-02-18 16:50:15 -0300

    Added -y flag to apt install and autoremove commands

--- Bash Scripts/Install Developer Packages.sh
+++ Bash Scripts/Install Developer Packages.sh
@@ -8,14 +8,17 @@ declare green="\033[1;32m"
 # Visual Studio Code
 echo -e "${yellow}==================================\n\n Installing Visual Studio Code... \n\n==================================${default}"
 sleep 2
+
 # check to see if Snap Store is installed
 declare isSnapInstalled=$(dpkg-query -W -f='${Status}' snapd 2>/dev/null | grep -c "ok installed")
+
 if [ $isSnapInstalled = 0 ]; then
 	echo "Snap Store was not found. Installing Snap..."
 	sudo apt-get update
-	sudo apt-get install snapd
+	sudo apt-get install snapd -y
 	sleep 2
 fi
+
 sudo snap install --classic code
 
 # Developer packages
@@ -27,7 +30,7 @@ declare -a devPkgs=(
 for pkg in "${devPkgs[@]}"; do
 	echo -e "${yellow}\n==================================\n\n Installing ${pkg} \n\n==================================${default}"
 	sleep 2
-	sudo apt-get install $pkg
+	sudo apt-get install $pkg -y
 done
 
 # NodeJS
@@ -37,7 +40,7 @@ sudo n latest
 
 echo -e "${yellow}\n*** Removing unused dependencies... ***\n${default}"
 sleep 2
-sudo apt autoremove
+sudo apt autoremove -y
 sudo apt-get clean
 
 echo -e "\n${green}ALL DONE! :)${default}\n"
