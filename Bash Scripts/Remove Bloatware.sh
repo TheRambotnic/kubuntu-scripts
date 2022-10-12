c606dde7516276f1383e17ede68b09496f441707
TheRambotnic <lucasrafaellonguini09@gmail.com>
2022-10-12 11:35:11 -0300

    Initial commit

---
+++ Bash Scripts/Remove Bloatware.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# text colors
+declare yellow="\033[1;33m"
+declare default="\033[1;0m"
+declare green="\033[1;32m"
+
+removeSystemBloatware() {
+	# bloatware packages
+	declare -a sysBloatware=(
+		elisa
+		kmahjongg
+		kmines
+		kpat
+		krdc
+		kwalletmanager
+		konversation
+		thunderbird*
+		skanlite
+	)
+
+	# loop through array and remove packages
+	for pkg in "${sysBloatware[@]}"; do
+		echo -e "${yellow}\n****************************"
+		echo -e " Uninstalling $pkg "
+		echo -e "****************************${default}"
+		sudo apt purge $pkg -y
+		sleep 1
+	done
+}
+
+removeSnapBloatware() {
+	# bloatware Snaps
+	declare -a snapBloatware=(
+		firefox
+	)
+
+	# check to see if Snap is installed
+	declare isSnapInstalled=$(dpkg-query -W -f='${Status}' snapd 2>/dev/null | grep -c "ok installed")
+	
+	if [ $isSnapInstalled = 0 ]; then
+		echo "Snap was not found. Installing Snap..."
+		sudo apt-get update
+		sudo apt-get install snapd -y
+		sleep 2
+	else
+		# loop through array and remove Snaps
+		for snapPkg in "${snapBloatware[@]}"; do
+			echo -e "${yellow}\n****************************"
+			echo -e " Uninstalling $snapPkg "
+			echo -e "****************************${default}"
+			sudo snap remove $snapPkg
+			sudo rm -rf ~/snap/$snapPkg
+			sleep 1
+		done
+	fi
+}
+
+removeBloatware() {
+	echo -e "${yellow}==============================="
+	echo -e " REMOVING BLOATWARE... "
+	echo -e "===============================${default}"
+	sleep 2
+
+	removeSystemBloatware
+	removeSnapBloatware
+
+	echo -e "${yellow}\n==============================="
+	echo -e " REMOVING DEPENDENCIES... "
+	echo -e "===============================${default}"
+	sleep 2
+	sudo apt autoremove -y
+	sudo apt-get clean
+
+	echo -e "${green}\nALL DONE! :)\n${default}"
+
+	# pause execution
+	read -p "" opt
+	case $opt in
+		* ) exit;;
+	esac
+}
+
+echo -e "\033]2;Remove Bloatware\007"
+echo -e "This file will remove a few pre-installed packages from your computer and you will be prompted for your superuser password in order to do so.\n\n"
+
+while true; do
+	read -p "Do you wish to continue? [y/n]: " yn
+	case $yn in
+		[Nn]* ) exit;;
+		[Yy]* ) removeBloatware;;
+		* ) echo -e "\nPlease select yes (Y) or no (N). ";;
+	esac
+done
---
+++ README.md
@@ -0,0 +1,41 @@
+# Bash Scripts for [Kubuntu 22.04.1 LTS](https://kubuntu.org/)
+These are some bash scripts I've made to make my life easier when installing/removing packages because I seriously can't be bothered to use the terminal to do shit manually... 😓
+
+## Included:
+* Remove Bloatware.sh
+* Install Essentials.sh
+* Install Opera Browser.sh
+* Install Developer Packages.sh
+* [scrcpy.sh](https://github.com/Genymobile/scrcpy)
+
+**HOW TO USE:** Right click the file and click `Run In Konsole`.
+
+## Keyboard Shortcuts
+`Meta` in this case would be the Windows logo key.
+
+### Applications
+* **Dolphin**
+	* Shortcut: `Meta + E`
+* **KRunner**
+	* Shortcut: `Meta + R`
+* **Spectacle**
+	* Capture Rectangular Region: `Meta + Shift + S`
+* **System Monitor**
+	* Shortcut: `Ctrl + Shift + Esc`
+* **System Settings**
+	* Shortcut: `Meta + I`
+
+### System Services
+* **Plasma**
+	* Activate Minimize all Windows Applet: `Meta + M`
+	* Open Klipper at Mouse Position: `Meta + V`
+* **Session Management**
+	* Lock Session: `Meta + L`
+
+### Dolphin
+* **Create Folder**
+	* Shortcut: `Ctrl + Shift + N`
+* **Editable Location**
+	* Shortcut: `F4`
+* **Terminal**
+	* Shortcut: `NONE`
