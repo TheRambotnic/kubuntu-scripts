fcac1d7971aa717067420d15dce76a53d90dce21
TheRambotnic <lucasrafaellonguini09@gmail.com>
2024-08-28 11:43:13 -0300

    Added fzf to the essential packages

--- Bash Scripts/Install Essentials.sh
+++ Bash Scripts/Install Essentials.sh
@@ -11,6 +11,7 @@ installPkgs() {
 	sudo apt install curl
 
 	curlDownloads
+	gitDownloads
 	setupRepositories
 
 	sudo nala update
@@ -133,6 +134,13 @@ curlDownloads() {
 
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
+gitDownloads() {
+	# FZF (Command-line fuzzy finder)
+	displayInstallMessage "fzf"
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
 }
 
 setupRepositories() {
