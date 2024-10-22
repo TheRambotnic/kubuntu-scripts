272e1d6e2c1a3b8a4298ca397163ffc81b86f7c0
TheRambotnic <lucasrafaellonguini09@gmail.com>
2024-10-22 20:30:55 -0300

    Added lualine to the plugins list and changed the status bar to display rounded separators

--- Shell Configs/lua/plugins.lua
+++ Shell Configs/lua/plugins.lua
@@ -12,7 +12,48 @@ vim.call("plug#begin")
 	Plug("nvim-telescope/telescope.nvim", { branch = "0.1.x" }) -- Fuzzy file finder
 	Plug("gelguy/wilder.nvim") -- Command auto completion
     Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
+	Plug("nvim-lualine/lualine.nvim")
 vim.call("plug#end")
 
 require("wilder").setup({ modes = {":", "/", "?"} })
 require("nvim-tree").setup()
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+})
