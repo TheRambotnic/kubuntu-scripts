8249fb17df39b9792ccef942adeed759b7f8d6a7
TheRambotnic <lucasrafaellonguini09@gmail.com>
2025-03-27 17:37:07 -0300

    Removed Firefox repository and added Brave Browser to essentials list

--- Bash Scripts/Install Essentials.sh
+++ Bash Scripts/Install Essentials.sh
@@ -18,7 +18,6 @@ installPkgs() {
 
 	# Essential packages
 	declare -a essentialsApt=(
-		firefox
 		gimp
 		qalculate-gtk
 		wine
@@ -33,6 +32,7 @@ installPkgs() {
 		qdirstat
         mangohud
         goverlay
+        brave-browser
 	)
 
 	for pkg in "${essentialsApt[@]}"; do
@@ -148,21 +148,6 @@ gitDownloads() {
 setupRepositories() {
 	# Fastfetch
 	sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
-
-	# Firefox
-	sudo install -d -m 0755 /etc/apt/keyrings
-
-	wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-
-	gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-
-	echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-
-	echo '
-	Package: *
-	Pin: origin packages.mozilla.org
-	Pin-Priority: 1000
-	' | sudo tee /etc/apt/preferences.d/mozilla
 }
 
 removeDependencies() {
